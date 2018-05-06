306b560aebb658f66d8f05f27d49236b842705e9
Matic Jazbec <mjazbec1@gmail.com>
2018-05-06 19:32:38 +0200

    added 2016-2017 songs

--- data.py
+++ data.py
@@ -38,6 +38,7 @@ class Billboard:
     wikiUrl = 'https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_{}'
     metroLyricsRootUrl = 'http://www.metrolyrics.com/{0}'
     metroLyricsSearch = 'http://api.metrolyrics.com/v1//multisearch/all/X-API-KEY/196f657a46afb63ce3fd2015b9ed781280337ea7/format/json?find={}'
+    geniusSearchUrl = 'https://genius.com/api/search/multi?q={0}'
 
     def get_song_list(self, year):
         yearUrl = self.wikiUrl.format(year)
@@ -55,29 +56,68 @@ class Billboard:
         return songList
 
     def extract_lyrics(self, lyricsNode):
-        return '\n'.join(verse.text for verse in lyricsNode.find_all('p', {'class' : 'verse'}))
+        return ' '.join(verse.text for verse in lyricsNode.find_all('p', {'class' : 'verse'}))
 
-    def search_lyrics(self, track, artist):
+    def search_lyrics_metro(self, track, artist):
         track = track.replace('-', '+').lower()
         artist = artist.replace('-', '+').lower()
+
+        if 'featuring' in artist:
+            artist = artist[0:artist.index('featuring')-1]
+
         r = requests.get(self.metroLyricsSearch.format(track+'+'+artist))
 
         jsonResult = json.loads(r.text)
+
+        if(artist.replace('-', ' ') not in jsonResult['results']['lyrics']['d'][0]['p'].lower()):
+            return
         url = jsonResult['results']['lyrics']['d'][0]['u']
 
         lyricsDiv = self.get_lyrics_div(url)
 
         return self.extract_lyrics(lyricsDiv)
+    
+    def search_lyrics_genius(self, track, artist):
+        track = track.replace('-', '+').lower()
+        artist = artist.replace('-', '+').lower()
+
+        if 'featuring' in artist:
+            artist = artist[0:artist.index('featuring')-1]
+
+        r = requests.get(self.geniusSearchUrl.format(track+'+'+artist))
+
+        jsonResult = json.loads(r.text)
+
+        if(artist.replace('+',' ') not in jsonResult['response']['sections'][0]['hits'][0]['result']['full_title'].replace(u'\xa0', u' ').lower()):
+            return
+        url = 'https://genius.com'+jsonResult['response']['sections'][0]['hits'][0]['result']['path']
+
+        return self.extract_lyrics_genius(url)
+
 
     def get_lyrics_div(self, url):
         metroUrl = str.format(self.metroLyricsRootUrl, url)
         r = requests.get(metroUrl)
         soup = BeautifulSoup(r.text, 'html.parser')
         return soup.find('div', {'id':'lyrics-body-text'})
+    
+    def extract_lyrics_genius(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        lyricsDiv = soup.find('div', {'class':'lyrics'})
+
+        lyrics = ' '.join([a.text for a in lyricsDiv.find_all('a') if a.text[0] != '['])
+
+        return lyrics
+
+
 
     def get_song_lyrics(self, track, artist):
         track = track.replace(' ', '-').lower()
         artist = artist.replace(' ', '-').lower()
+
+        artist = artist.replace('&', 'and')
+        track = track.replace('&', 'and').replace('\'','')
         
         lyricsDiv = self.get_lyrics_div(track+'-lyrics-'+ artist+'.html')
 
@@ -85,8 +125,15 @@ class Billboard:
         if lyricsDiv:
             return self.extract_lyrics(lyricsDiv)
 
-        #If url doesn't exist, try search
-        return self.search_lyrics(track, artist)
+        #If url doesn't exist, try searching lyrics on MetroLyrics
+        lyrics = self.search_lyrics_metro(track, artist)
+        if lyrics:
+            return lyrics
+        
+        #Try searcing on genius.com
+        return self.search_lyrics_genius(track,artist)
+        
+
     
 
 
--- main.py
+++ main.py
@@ -17,5 +17,23 @@ api = LastFm()
 
 bb = Billboard()
 
-html = bb.get_song_lyrics('Never be like you', 'Flume featuring Kai')
-print(html)
+# html = bb.get_song_lyrics('Never be like you', 'Flume featuring Kai')
+# print(html)
+
+destination = open('data/newlyrics.csv', 'a', encoding = 'utf-8')
+with open('data/newsongs.csv', 'r') as csvfile:
+    songs = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for i, row in enumerate(songs):
+        if i < 116:
+            continue
+
+        artist = row[3]
+        track = row[2]
+        lyrics = bb.get_song_lyrics(track,artist)
+
+        if not lyrics:
+            lyrics = "NA"
+        line = row[1]+',"'+track+'","'+artist+'",'+row[0]+',"'+lyrics.replace('\"', '').replace('\n',' ')+'\n'
+
+        destination.write(line)
+
