2ef0c23175ade123a0e223e6d9a005baf83e8cbd
Matic Jazbec <mjazbec1@gmail.com>
2018-05-27 22:50:12 +0200

    attemp on fixing poorly parsed lyrics with wordninja

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data import LastFm, Billboard, TagClassifier
 import csv
+import wordninja
 
 
 # with open('data/billboard_lyrics_1964-2015.csv', 'r') as csvfile:
@@ -16,42 +17,60 @@ api = LastFm()
 # html = bb.get_song_lyrics('Never be like you', 'Flume featuring Kai')
 # print(html)
 
-destination =  open('data/billboard_lyrics_1964-2017_tagged.csv', 'a', encoding = 'utf-8')
-originalData = [line for line in open('data/billboard_lyrics_1964-2017_tagged.csv', 'r',  encoding = 'utf-8')][1:]
-songs = csv.reader(originalData, delimiter=',', quotechar='"' )
+originalData = [line for line in open('data/preprocessed.csv', 'r',  encoding = 'utf-8')][1:]
+destination =  open('data/preprocessed_fixed.csv', 'a', encoding = 'utf-8')
+songs = csv.reader(originalData, delimiter=';', quotechar='"' )
 
+def fix_parsing_fuckups(songs, destination):
+     for i, row in enumerate(songs):
+        lyrics = row[4]
+        splitWords = wordninja.split(lyrics)
 
-tc = TagClassifier()
-
-for i, row in enumerate(songs):
-
-    tags = row[6:12]
-    era =''
-    year = int(row[3])
-    
-    tag = 'other'
-    if all(tag == 'NA' for tag in tags):
-        tag = 'NA'
-    else:
-        g = tc.get_genre(tags)
-        if g:
-            tag = g
-
-    
-    if year < 1970:
-        era = '60s'
-    elif year < 1980:
-        era = '70s'
-    elif year < 1990:
-        era = '80s'
-    elif year < 2000:
-        era = '90s'
-    elif year < 2010:
-        era = '2000s'
-    elif year < 2020:
-        era = '2010s'
+        if 'miscellaneous' in splitWords:
+            miscIndex = splitWords.index('miscellaneous')
+            if miscIndex < 5:
+                splitWords = splitWords[miscIndex +1 :]
         
-    
-    line = originalData[i].strip()+',' +tag + ',' +era + '\n'
-    destination.write(line)
+        fixed = ' '.join(splitWords)
+        # print(len(lyrics), ' ', len(fixed))
 
+        line = originalData[i].replace(lyrics, fixed)
+        destination.write(line)
+
+def set_genres(songs, destination):
+    tc = TagClassifier()
+
+    for i, row in enumerate(songs):
+
+        tags = row[6:12]
+        era =''
+        year = int(row[3])
+        
+        tag = 'other'
+        if all(tag == 'NA' for tag in tags):
+            tag = 'NA'
+        else:
+            g = tc.get_genre(tags)
+            if g:
+                tag = g
+
+        
+        if year < 1970:
+            era = '60s'
+        elif year < 1980:
+            era = '70s'
+        elif year < 1990:
+            era = '80s'
+        elif year < 2000:
+            era = '90s'
+        elif year < 2010:
+            era = '2000s'
+        elif year < 2020:
+            era = '2010s'
+            
+        
+        line = originalData[i].strip()+',' +tag + ',' +era + '\n'
+        destination.write(line)
+
+
+fix_parsing_fuckups(songs,destination)
